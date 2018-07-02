227397d631f8f00e7fbe87a36aa44075f488bfcd
Asep Sholihin <asep.sholihin11@gmail.com>
2018-07-02 21:27:45 +0700

    style login

--- application/views/V_Login.php
+++ application/views/V_Login.php
@@ -2,24 +2,40 @@
 <head>
 	<title>Login</title>
 	<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet" type="text/css" />
+	<link href="<?php echo base_url(); ?>assets/css/util.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<h1>Login SIAKAD</h1>
-	<form action="<?php echo base_url('login/proses_login'); ?>" method="post">		
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name="username"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name="password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Login"></td>
-			</tr>
-		</table>
-	</form>
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(<?php echo base_url(); ?>assets/images/bg-01.jpg);">
+					<span class="login100-form-title-1">
+						Sign In
+					</span>
+				</div>
+
+				<form class="login100-form validate-form" action="<?php echo base_url('login/proses_login'); ?>" method="post">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+						<span class="label-input100">Username</span>
+						<input class="input100" type="text" name="username" placeholder="Enter username">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="password" placeholder="Enter password">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
