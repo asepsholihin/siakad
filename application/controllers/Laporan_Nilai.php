b5e4f8fa4fafd382d7378bf86f470e8ab579d740
Asep Sholihin <asep.sholihin11@gmail.com>
2018-07-07 23:52:44 +0700

    laporan

--- application/controllers/Laporan_Nilai.php
+++ application/controllers/Laporan_Nilai.php
@@ -38,5 +38,89 @@ class Laporan_Nilai extends MY_Controller {
 	public function download($semester) {
 		$data['mahasiswa'] = $this->db->query("SELECT * FROM mahasiswa LEFT JOIN nilai ON nilai.id_mahasiswa = mahasiswa.nim WHERE mahasiswa.semester='".$semester."' GROUP BY mahasiswa.nim")->result();
 		$this->load->view('pages/laporan_nilai/v_print_nilai', $data);   
-    }
+	}
+	
+	public function createXLS($id_matkul,$semester) {
+		$this->load->library('excel');
+		$objPHPExcel = new PHPExcel();	
+	
+		if($this->session->userdata('role') == "dosen") {	
+			$where = "nilai.id_dosen LIKE '%".$this->session->userdata("id_user")."%' AND ";	
+		} else if($this->session->userdata('role') == "dosen_wali") {	
+			$where = "mahasiswa.id_dosen LIKE '%".$this->session->userdata("id_user")."%' AND ";	
+		} else if($this->session->userdata('role') == "kajur") {	
+			$join = "JOIN dosen ON dosen.id_prodi = mahasiswa.id_prodi";	
+			$group = "GROUP BY nilai.id_matkul";	
+		}	
+	
+		$data = $this->db->query("	
+		SELECT	
+		mahasiswa.nim,	
+		mahasiswa.nama,	
+		matkul.nama as matkul, matkul.sks,	
+		nilai.uts, nilai.uas, nilai.tugas,	
+		(CASE WHEN nilai.uts IS NOT NULL THEN nilai.uts*kriteria.uts/100 ELSE '' END) AS total_uts,	
+		(CASE WHEN nilai.uas IS NOT NULL THEN nilai.uas*kriteria.uas/100 ELSE '' END) AS total_uas,	
+		(CASE WHEN nilai.tugas IS NOT NULL THEN nilai.tugas*kriteria.tugas/100 ELSE '' END) AS total_tugas	
+		FROM nilai 	
+		JOIN matkul ON matkul.id = nilai.id_matkul	
+		RIGHT JOIN mahasiswa ON nilai.id_mahasiswa = mahasiswa.nim	
+		".$join."	
+		JOIN kriteria ON nilai.id_dosen = kriteria.id_dosen AND nilai.id_matkul='".$id_matkul."' WHERE ".$where." nilai.semester='".$semester."' ".$group."
+		GROUP BY mahasiswa.nim");	
+	
+		$tambah_field = array('NIM','Nama', 'Mata Kuliah', 'SKS', 'UTS', 'UAS', 'Tugas', 'Nilai Akhir', 'Nilai Mutu');	
+		$tambah = array('nilai_akhir', 'nilai_mutu');	
+		$fields = array_merge($data->list_fields(), $tambah);	
+			
+		unset($fields[7],$fields[8],$fields[9]);	
+			
+		$col = 0;	
+		foreach ($tambah_field as $field)	
+		{	
+			//echo json_encode($field);	
+			$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, 1, $field);	
+			$col++;	
+		}	
+ 	
+			
+			
+		$row = 2;	
+			
+		foreach($data->result() as $value)	
+		{	
+				
+			$value->nilai_akhir = $value->total_uts+$value->total_uas+$value->total_tugas;	
+			$value->nilai_mutu = $this->M_Nilai->grading($value->nilai_akhir);	
+			unset($value->total_uts, $value->total_uas, $value->total_tugas);	
+				
+				
+			$col = 0;	
+			foreach ($fields as $field)	
+			{	
+				//echo json_encode($value->$field);	
+				$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, $row, $value->$field);	
+				$col++;	
+			}	
+ 	
+			$row++;	
+		}	
+		$objPHPExcel->setActiveSheetIndex(0);	
+	
+		$objPHPExcel->getActiveSheet()->setTitle('Nilai Semester '.$semester);	
+	
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');	
+	
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");	
+		header("Cache-Control: no-store, no-cache, must-revalidate");	
+		header("Cache-Control: post-check=0, pre-check=0", false);	
+		header("Pragma: no-cache");	
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');	
+	
+		$matkul = $this->db->get_where('matkul', array('id' => $id_matkul))->row();	
+	
+		header('Content-Disposition: attachment;filename="Nilai-'.$matkul->nama.'-Semester-'.$semester.'.xlsx"');	
+	
+		$objWriter->save("php://output");      	
+	}
 }
--- application/views/pages/laporan_nilai/V_Laporan_Nilai.php
+++ application/views/pages/laporan_nilai/V_Laporan_Nilai.php
@@ -29,7 +29,13 @@
                         echo form_dropdown('id_matkul', $matkul, $this->uri->segment(3), $js);
                     ?>
                 </div>
+                <?php if($this->session->userdata('role') == 'wadir1') { ?>
                 <a href="javascript: w=window.open('<?php echo base_url('laporan_nilai'); ?>/download/'+$('#semester').val()+''); w.print();" class="btn btn-outline-success pull-right">Download</a>
+                <?php } else { 
+                    if($this->uri->segment(3)) { ?>
+                    <a href="<?php echo base_url('laporan_nilai') ?>/createXLS/<?php echo $this->uri->segment(3); ?>/<?php echo $this->uri->segment(4); ?>" class="btn btn-outline-success pull-right">Download</a>
+                <?php } 
+                }?>
 
                 <div class="table-responsive">
                     <table id="data" class="display table pt-3">
